75e9bffe1124f0eee0b73e7c5355fa72564f0952
neko01t <omsonawane124421@gmail.com>
2026-06-22 00:50:28 +0530

    Reusable to move items

--- bash/cyber-cleand.sh
+++ bash/cyber-cleand.sh
@@ -4,20 +4,29 @@ debugRevert() {
   mv ./testPictureFolder/*.png ./testPictureFolder/*.jpg ./
 }
 
-ls=(*)
-shopt -s nullglob
-pic=(*.png *.jpg)
-doc=(*.txt *png)
-shop -u nullglob
-if [[ ${#pic[@]} -ne 0 ]]; then
-  echo "Debug : ${pic[@]}"
-  mv ${pic[@]} ./testPictureFolder/
+move() {
+  dest=$1
+  files=("${@:2}")
+  echo "Debug : ${files[@]}"
+  mv "${files[@]}" "${dest}"
   echo " -------- "
-  echo "./testPictureFolder/"
-  ls ./testPictureFolder/
-  echo " -------- "
+  echo "${dest}"
+  ls "${dest}"
   echo "Debug: Reverting back "
   debugRevert
+}
+
+shopt -s nullglob
+pic=(*.png *.jpg)
+doc=(*.txt *.png)
+arr=("doc" "pic")
+shopt -u nullglob
+for target in "${arr[@]}"; do
+  declare -n curr=$target
+done
+if [[ ${#pic[@]} -ne 0 ]]; then
+  # move "./testPictureFolder/" "${pic[@]}"
+  echo ""
 fi
 
 # debugRevert
