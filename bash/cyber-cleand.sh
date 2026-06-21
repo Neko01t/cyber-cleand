b078ba3a03f85932124b30c86caff896f641158e
neko01t <omsonawane124421@gmail.com>
2026-06-22 00:33:26 +0530

    Pictures moves safely to the destination

--- bash/cyber-cleand.sh
+++ bash/cyber-cleand.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+debugRevert() {
+  mv ./testPictureFolder/*.png ./testPictureFolder/*.jpg ./
+}
+
 ls=(*)
-echo "${ls[2]}"
+shopt -s nullglob
+pic=(*.png *.jpg)
+doc=(*.txt *png)
+shop -u nullglob
+if [[ ${#pic[@]} -ne 0 ]]; then
+  echo "Debug : ${pic[@]}"
+  mv ${pic[@]} ./testPictureFolder/
+  echo " -------- "
+  echo "./testPictureFolder/"
+  ls ./testPictureFolder/
+  echo " -------- "
+  echo "Debug: Reverting back "
+  debugRevert
+fi
+
+# debugRevert
