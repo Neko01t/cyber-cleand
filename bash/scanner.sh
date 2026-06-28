dfc4a9f8b0506df3784b737f7105b7b2a0665cdb
neko01t <omsonawane124421@gmail.com>
2026-06-28 20:29:59 +0530

    It worked !!! yay

--- bash/scanner.sh
+++ bash/scanner.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-WATCH_DIR="/home/oms/Downloads"
-inotifywait -m -e close_write,moved_to --format '%w%f' "$WATCH_DIR" | while read -r NEW_FILE; do
-  echo "Detected new file $NEW_FILE"
+WATCH_DIRS=(
+  "/home/oms/Documents/dele/cyber-cleand/bash/"
+  "/home/oms/Documents/dele/cyber-cleand/rust/"
+)
+
+inotifywait -m -e close_write,moved_to --format '%w%f' \
+  --exclude '.*\.tmp$|cyber-cleand\.sh' "${WATCH_DIRS[@]}" | while read -r NEW_FILE; do
+
+  echo "Detected change: $NEW_FILE"
+  ./cyber-cleand.sh . prod
 
 done
