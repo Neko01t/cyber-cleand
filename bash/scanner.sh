5f421ae46ad4e419ba31b12c579cf915d2143959
neko01t <omsonawane124421@gmail.com>
2026-06-28 21:01:11 +0530

    setting config for scanner

--- bash/scanner.sh
+++ bash/scanner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-WATCH_DIRS=(
-  "/home/oms/Documents/dele/cyber-cleand/bash"
-  "/home/oms/Documents/dele/cyber-cleand/rust"
-)
+source ./config/rules.conf || {
+  echo "Error: rules.conf not found!"
+  exit 1
+}
 
 inotifywait -m -e close_write,moved_to --format '%w' \
   --exclude '.*\.tmp$|cyber-cleand\.sh' "${WATCH_DIRS[@]}" | while read -r NEW_FILE; do
